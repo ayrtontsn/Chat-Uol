6ecfe3a11150334a92ce884c87147dc16f718533
Ayrton Tales Silva do Nascimento <ayrtontsn@hotmail.com>
2024-07-25 12:37:57 -0300

    feat: Send message to chat

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
+const name = "João";
+let people_send = "Todos";
+let target_send = "Público";
+
+
+
+
 function show_menu(){
     const menu = document.querySelector(".menu");
     const mask = document.querySelector(".mask");
@@ -14,8 +21,55 @@ function hidden_menu(){
     mask.classList.add("hidden")
 }
 
-const name = "João"
+function visibility(click){
+
+    const clicked = click.childNodes[3];
+    target_send = click.childNodes[1];
+    target_send = target_send.childNodes[3].innerHTML
+    const before = document.querySelector(".visibility .select")
+    
+    before.classList.remove("select")
+    before.classList.add("hidden")
+
+    clicked.classList.remove("hidden")
+    clicked.classList.add("select")
+}
+
+function people(click){
+
+    const clicked = click.childNodes[3];
+    people_send = click.childNodes[1];
+    people_send = people_send.childNodes[3].innerHTML
+    const before = document.querySelector(".people .select")
+    
+    before.classList.remove("select")
+    before.classList.add("hidden")
+
+    clicked.classList.remove("hidden")
+    clicked.classList.add("select")
+}
 
 function send(){
-    
+    const input = document.querySelector("input").value
+    const ul = document.querySelector("ul")
+
+    if (target_send === "Público"){
+        ul.innerHTML += `        
+                <li>
+                    <h6>
+                        <em>(12:12:12)</em> <strong>${name}</strong> para <strong>${people_send}</strong>: ${input}
+                    </h6>
+                </li>
+                `  
+    }
+    else{
+        ul.innerHTML += `        
+        <li class="priv">
+            <h6>
+                <em>(12:12:12)</em> <strong>${name}</strong> reservadamente para <strong>${people_send}</strong>: ${input}
+            </h6>
+        </li>
+        `  
+    }
+    document.querySelector("input").value = ""
 }
